3ede37fc2a4ff7ceb7fb9d7faa37838ea1b55a5b
kirtidalave <dalavekirti@gmail.com>
2022-03-29 15:56:15 +0530

    skipeed successfully

--- test_calculator.py
+++ test_calculator.py
@@ -1,26 +1,23 @@
 import calculator
 import pytest
 
+@pytest.mark.skip
 @pytest.mark.parametrize("a,b,c",[(93,2,5),(10,12,15),(2,5,8),(7,8,15)])
 def test_add(a,b,c):
-
     result = calculator.add(a,b)
     assert c==result
 
-def test_sub():
-    x = 20
-    y = 12
-    result = calculator.substract(x,y)
-    assert x-y==result
+@pytest.mark.parametrize("a,b,c",[(7,2,5),(27,12,15),(13,5,8),(7,8,15)])
+def test_sub(a,b,c):
+    result = calculator.substract(a,b)
+    assert c==result
 
-def test_mult():
-    x = 15
-    y = 20
-    result = calculator.multiply(x,y)
-    assert x*y==result
+@pytest.mark.parametrize("a,b,c",[(1,2,2),(3,4,12),(11,12,23),(2,7,9)])
+def test_mult(a,b,c):
+    result = calculator.multiply(a,b,c)
+    assert c==result
 
-def test_div():
-    x = 12
-    y = 15
-    result = calculator.divide(x,y)
-    assert x/y==result
+@pytest.mark.parametrize("a,b,c",[(10,5,2),(5,2,2.5),(70,2,35),(10,3,5)])
+def test_div(a,b,c):
+    result = calculator.divide(a,b,c)
+    assert c==result
