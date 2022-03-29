a973faf2fa93b1bb4391b8eb1bbdde70bee2a56f
kirtidalave <dalavekirti@gmail.com>
2022-03-29 16:24:09 +0530

    xfailed successfully

--- test_calculator.py
+++ test_calculator.py
@@ -1,12 +1,13 @@
 import calculator
 import pytest
 
+
 @pytest.mark.skip
 @pytest.mark.parametrize("a,b,c",[(93,2,5),(10,12,15),(2,5,8),(7,8,15)])
 def test_add(a,b,c):
     result = calculator.add(a,b)
     assert c==result
-
+@pytest.mark.xfail
 @pytest.mark.parametrize("a,b,c",[(7,2,5),(27,12,15),(13,5,8),(7,8,15)])
 def test_sub(a,b,c):
     result = calculator.substract(a,b)
